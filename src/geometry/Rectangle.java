34aa4a19520c49c8320dd3a34d8ea175d4e80be1
ikm216 <ikmclassof22@gmail.com>
2024-12-10 20:46:36 -0500

    docs:added documentation feat to the rectangle class.

--- src/geometry/Rectangle.java
+++ src/geometry/Rectangle.java
@@ -4,16 +4,34 @@ public class Rectangle extends Geometry2D {
     double len;
     double wid;
 
+    /**
+     * Constructs a Rectangle with the specified length and width.
+     *
+     * @param len The length of the rectangle.
+     * @param wid The width of the rectangle.
+     */
     public Rectangle(double len, double wid) {
         this.len = len;
         this.wid = wid;
     }
 
+    /**
+     * Calculates the area of the rectangle.
+     * The formula for the area of a rectangle is length * width.
+     *
+     * @return The area of the rectangle.
+     */
     @Override
     public double area() {
         return len * wid;
     }
 
+    /**
+     * Calculates the perimeter of the rectangle.
+     * The formula for the perimeter of a rectangle is 2 * (length + width).
+     *
+     * @return The perimeter of the rectangle.
+     */
     @Override
     public double perimeter() {
         return 2 * (len + wid);
