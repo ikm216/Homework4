720214786f0a1b1152792260c7692e768c417910
ikm216 <ikmclassof22@gmail.com>
2024-12-10 21:02:18 -0500

    feat:added feature to the triangle class.

--- src/geometry/Triangle.java
+++ src/geometry/Triangle.java
@@ -1,5 +1,20 @@
 package geometry;
 
-public class Triangle {
-    
+public class Triangle extends Geometry2D{
+    double b;
+    double h;
+
+    public Triangle(double b, double h){
+        this.b = b;
+        this.h = h;
+    }
+
+    @Override
+    public double area() {
+        return 0.5 * b * h;
+    }
+
+    public double perimeter() {
+        return 3 * b;
+    }
 }
