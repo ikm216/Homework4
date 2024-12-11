69afa1811f6824957918692b27387780b7b928c6
ikm216 <ikmclassof22@gmail.com>
2024-12-10 19:58:34 -0500

    docs:added documentation to feature to the circle class.

--- src/geometry/Circle.java
+++ src/geometry/Circle.java
@@ -3,15 +3,32 @@ package geometry;
 public class Circle extends Geometry2D{
     double rad;
 
+    /**
+     * Constructs a Circle with the specified radius.
+     *
+     * @param rad The radius of the circle.
+     */
     public Circle(double rad) {
         this.rad = rad;
     }
 
+    /**
+     * Calculates the area of the circle.
+     * The formula for the area of a circle is π * radius^2.
+     *
+     * @return The area of the circle.
+     */
     @Override
     public double area(){
         return Math.PI * rad * rad;
     }
 
+    /**
+     * Calculates the perimeter (circumference) of the circle.
+     * The formula for the perimeter of a circle is 2 * π * radius.
+     *
+     * @return The perimeter (circumference) of the circle.
+     */
     @Override
     public double perimeter(){
         return 2 *Math.PI * rad;
