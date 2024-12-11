7f15c9ceeca71f4414fc054830b6d4de743ccf1e
ikm216 <ikmclassof22@gmail.com>
2024-12-10 22:06:45 -0500

    feat:added feature to the TriangularPrism class.

--- src/geometry/TriangularPrism.java
+++ src/geometry/TriangularPrism.java
@@ -1,5 +1,24 @@
 package geometry;
 
-public class TriangularPrism {
-    
+public class TriangularPrism extends Geometry3D{
+    double b;
+    double h;
+    double len;
+
+    public TriangularPrism(double b, double h, double len) {
+        this.b = b;
+        this.h = h;
+        this.len = len;
+    }
+
+    @Override
+    public double surfaceArea() {
+        double baseArea = 0.5 * b * h;
+        return 2 * baseArea + 3 * (b * len);
+    }
+
+    @Override
+    public double volume() {
+        return 0.5 * b * h * len;
+    }
 }
