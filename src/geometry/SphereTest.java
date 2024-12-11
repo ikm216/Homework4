1dc81255de12f3ce32b5b1f22e45e4bfb628d70a
ikm216 <ikmclassof22@gmail.com>
2024-12-10 21:19:25 -0500

    test:added tests to the sphere class.

---
+++ src/geometry/SphereTest.java
@@ -0,0 +1,23 @@
+package geometry;
+
+import org.junit.jupiter.api.Test;
+
+public class SphereTest {
+    @Test
+    void test_should_return_created_sphere(){
+        Sphere _ = new Sphere(5.0);
+    }
+
+    @Test
+    public void test_should_return_surface_area_of_sphere(){
+        Sphere sphere = new Sphere(5.0);
+        assert(sphere.surfaceArea() == (4.0 / 5.0) * Math.PI * Math.pow(5.0, 3));
+    }
+
+    @Test
+    public void test_should_return_volume_of_sphere(){
+        Sphere sphere = new Sphere(5.0);
+        assert(sphere.volume() == 4 * Math.PI * Math.pow(5.0, 2));
+        
+    }
+}
