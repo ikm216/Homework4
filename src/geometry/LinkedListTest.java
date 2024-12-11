d577f71cb45f5ee166732de69768e052a96840a1
ikm216 <ikmclassof22@gmail.com>
2024-12-10 22:40:23 -0500

    test:revamp tests to the Linked List class.

--- src/geometry/LinkedListTest.java
+++ src/geometry/LinkedListTest.java
@@ -7,27 +7,27 @@ import org.junit.jupiter.api.Test;
 public class LinkedListTest {
     
     @Test
-    void test_should_add_and_store(){
-        LinkedList<Geometry2D> link_list = new LinkedList<>();
+    void test_should_add_and_store() {
+        Linked_List<Geometry2D> link_list = new Linked_List<>();
         Rectangle rect = new Rectangle(5.0, 4.0);
         Circle circle = new Circle(5.0);
+
         link_list.add(rect);
         link_list.add(circle);
 
-
-
-
-        assertEquals(2, link_list.size());
-        assertEquals(rect, link_list.get(0));
-        assertEquals(circle, link_list.get(1));
+        assertEquals(2, link_list.size(), "Linked list size should be 2 after adding two elements.");
+        assertEquals(rect, link_list.find(0), "First element should be the rectangle.");
+        assertEquals(circle, link_list.find(1), "Second element should be the circle.");
     }
+
     @Test
-    void test_should_return_delete(){
-        LinkedList<Geometry2D> link_list = new LinkedList<>();
+    void test_should_remove_element() {
+        Linked_List<Geometry2D> link_list = new Linked_List<>();
         Rectangle rect = new Rectangle(5.0, 4.0);
         link_list.add(rect);
 
-        link_list.remove(rect);
-        assertEquals(0, link_list.size());
+        boolean removed = link_list.remove(rect);
+        assertEquals(0, link_list.size(), "Linked list size should be 0 after removing the element.");
+        assertEquals(true, removed, "Rectangle should be removed successfully.");
     }
 }
---
+++ src/geometry/Linked_List.java
@@ -0,0 +1,40 @@
+// package geometry;
+
+// import java.util.LinkedList;
+
+
+// public class Linked_List<T> {
+//     private LinkedList<T> link_list;
+
+    
+//     public Linked_List() {
+//         link_list = new LinkedList<>();
+//     }
+
+    
+//     public void add(T geometry) {
+//         link_list.add(geometry);
+//     }
+
+//     public boolean remove(T geometry) {
+//         return link_list.remove(geometry);
+//     }
+
+//     public T find(int index) {
+//         if (index >= 0 && index < link_list.size()) {
+//             return link_list.get(index);
+//         }
+//         return null;
+//     }
+
+    
+//     public void displayAll() {
+//         for (T geometry : link_list) {
+//             System.out.println(geometry);
+//         }
+//     }
+
+//     public int size() {
+//         return link_list.size();
+//     }
+// }
